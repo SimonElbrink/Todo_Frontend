ead3c3e118f4e0842a0f846695b43fd415daff3c
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-12-12 12:32:03 +0300

    Fetch and display users in `User` component using `getAllUsers` service method with `useEffect` integration.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,24 @@
-import {useState} from "react";
+import {useEffect, useState, useRef} from "react";
+import {getAllUsers} from "../services/userService.js";
 
 
 const User = () => {
 
     const [user, setUser] = useState([]);
     const [formData, setFormData] = useState({name: '', username: '', password: '', email: '', confirmPassword: ''});
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await getAllUsers();
+                setUser(data);
+            } catch (err) {
+                console.log("Failed to load users", err);
+            }
+        }
+            fetchData();
+
+    }, []);
 
     return (
         <div>User</div>
